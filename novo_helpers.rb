54328cbe9b490bafc26cc8ed4b318ac3def02aa5
Alex Jensen <96dalex@gmail.com>
2014-11-05 14:43:08 -0700

    added helpers module

---
+++ novo_helpers.rb
@@ -0,0 +1,37 @@
+module NovoHelpers
+  def find_products
+    PRODUCTS.select_all
+  end
+
+  def find_african_products
+    PRODUCTS.select_all_by_region("Africa")
+  end
+
+  def find_central_am_products
+    PRODUCTS.select_all_by_region("Central America")
+  end
+
+  def find_south_am_products
+    PRODUCTS.select_all_by_region("South America")
+  end
+
+  def find_pacific_products
+    PRODUCTS.select_all_by_region("Pacific")
+  end
+
+  def find_alternative_products
+    PRODUCTS.select_all_by_that_are_not("regular", "accessories", "subscription")
+  end
+
+  def find_accessories
+    PRODUCTS.select_all_by_type("accessories")
+  end
+
+  def update_product(id, params)
+    PRODUCTS.update(id, params)
+  end
+
+  def delete_product(id)
+    PRODUCTS.delete(id)
+  end
+end
